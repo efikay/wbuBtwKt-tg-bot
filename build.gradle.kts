d2cb6a1bc14650fca498420b6aade4167ec53d83
Efi Kay <efikay@icloud.com>
2024-08-08 08:21:41 +0300

    Remove dead import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
-
 plugins {
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
