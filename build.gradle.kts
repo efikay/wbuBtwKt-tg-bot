6d904ce03f30396489f729dea0ebfaa4fba77119
Efi Kay <efikay@icloud.com>
2024-08-08 08:20:55 +0300

    Add libraries for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.9.1")
 }
 
 kotlin {
