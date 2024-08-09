8389a0a4221c621ddbf0d5cce40b3763436c5f82
Efi Kay <efikay@icloud.com>
2024-08-09 12:58:35 +0300

    Remove unused random service config

--- src/main/kotlin/com/efikay/wbubtw/random/service/RandomServiceConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.efikay.wbubtw.random.service
-
-class RandomServiceConfig {
-    companion object {
-        // TODO: Add variable range of random (on random service construction)
-        const val MIN_RANDOM_NUMBER = 1
-        const val MAX_RANDOM_NUMBER = 100
-    }
-}
